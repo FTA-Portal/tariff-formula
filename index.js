7960bbc3d0c895dce9bae43b885b84f8ae0c4a52
GOD <nahid.akbar@data61.csiro.au>
2016-10-22 10:15:53 +1100

    fix problems with brfs

--- index.js
+++ index.js
@@ -8,7 +8,7 @@
 
 "use strict";
 
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -63,11 +63,11 @@ var signatureHelper = function signatureHelper(tree) {
       case 'minimum':
       case 'maximum':
       case 'range':
-        return ({
+        return {
           'minimum': 'm',
           'maximum': 'M',
           'range': 'r'
-        })[tree.operation] + "(" + tree.group.map(signatureHelper).join(',') + ")" + suffix;
+        }[tree.operation] + "(" + tree.group.map(signatureHelper).join(',') + ")" + suffix;
     }
   }
 
@@ -265,7 +265,7 @@ var evaluateHelper = function evaluateHelper(tree, variables) {
   return result;
 };
 
-var Formula = (function () {
+var Formula = function () {
   function Formula(text) {
     _classCallCheck(this, Formula);
 
@@ -295,7 +295,7 @@ var Formula = (function () {
   }]);
 
   return Formula;
-})();
+}();
 
 ;
 
